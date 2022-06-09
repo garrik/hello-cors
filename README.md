8070321eda036d57839d18ed84e2df27a7fdae31
garrik <federico.perotto@tiscali.it>
2022-06-10 01:57:06 +0200

    simple api to test cors

---
+++ README.md
@@ -0,0 +1,8 @@
+# test cors
+
+# dependencies
+pip3 install --user flask flask-cors
+
+# run
+export FLASK_APP=hello
+python3 -m flask run --port 5001 --host=0.0.0.0
---
+++ hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+#app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@app.route("/cors")
+#@cross_origin() # perchee errore se lo scommento???
+def hello_world_cors():
+    return "Hello, Cross Origin World!"
+
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+# https://stackoverflow.com/questions/63290047/flask-csp-content-security-policy-best-practice-against-attack-such-as-cross
+@app.after_request
+def add_security_headers(resp):
+    resp.headers['Content-Security-Policy']='default-src \'self\' http://*'
+    return resp
