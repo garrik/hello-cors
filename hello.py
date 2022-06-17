037d3cddc9bf6df5636956cf5d6035332ccec680
Federico Perotto <federico.perotto@quintetto.it>
2022-06-17 11:35:43 +0200

    fix calling fetch from chrome console
    console error is:
    Refused to connect to 'http://localhost:5001/' because it violates the following Content Security Policy directive: "default-src 'self' http://*". Note that 'connect-src' was not explicitly set, so 'default-src' is used as a fallback.

--- hello.py
+++ hello.py
@@ -15,8 +15,9 @@ def hello_world_cors():
     return "Hello, Cross Origin World!"
 
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+# https://web.dev/csp/
 # https://stackoverflow.com/questions/63290047/flask-csp-content-security-policy-best-practice-against-attack-such-as-cross
 @app.after_request
 def add_security_headers(resp):
-    resp.headers['Content-Security-Policy']='default-src \'self\' http://*'
+    resp.headers['Content-Security-Policy']='default-src \'self\' http://*; connect-src *;'
     return resp
