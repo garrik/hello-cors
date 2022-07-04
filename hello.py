8de051efd5ba3efd360f645f2034ed56400ce484
Federico Perotto <federico.perotto@quintetto.it>
2022-07-04 17:16:36 +0200

    add hello url to test POST

--- hello.py
+++ hello.py
@@ -1,10 +1,20 @@
 from flask import Flask
 
+import flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+@app.route("/hello", methods = ['POST'])
+def hello():
+    data = flask.request.form
+
+    name = data['name'] if 'name' in data else "anonymous"
+
+    return f"Hello, {name}!"
 
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
 # https://web.dev/csp/
