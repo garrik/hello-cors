59c676d0fbc4d37e53e7e6325bb6de8aea08701f
garrik <federico.perotto@tiscali.it>
2022-07-07 00:04:41 +0200

    parse data according to content type

--- hello.py
+++ hello.py
@@ -10,11 +10,35 @@ def hello_world():
 
 @app.route("/hello", methods = ['POST'])
 def hello():
-    data = flask.request.form
 
-    name = data['name'] if 'name' in data else "anonymous"
+    content_type = flask.request.headers.get('Content-Type')
+    print(f"Content-Type {content_type}")
+    content_type = content_type.lower() if content_type is not None else ''
+
+    if content_type.startswith('application/x-www-form-urlencoded'):
+        
+        data = flask.request.form
+        name = data['name'] if 'name' in data else "anonymous"
+
+    elif content_type.startswith('multipart/form-data'):
+        
+        data = flask.request.form
+        name = data['name'] if 'name' in data else "anonymous"
+
+    elif content_type.startswith('application/json'):
+        import json
+
+        data = flask.request.get_json() # TODO: check valid json
+        name = data['name'] if 'name' in data else "anonymous"
+
+    elif content_type.startswith('text/plain'):
+        name = flask.request.data
+
+    else:
+        return "content type not accepted", 400
 
     return f"Hello, {name}!"
+
 
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
 # https://web.dev/csp/
