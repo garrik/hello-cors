bdfe5f0a913e3d08f7b9abd745171ccb8fd5f46f
garrik <federico.perotto@tiscali.it>
2022-07-11 23:53:22 +0200

    fix hello-cors

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 # dependencies
 pip3 install --user flask flask-cors
 
-# run
+# test same origin
 export FLASK_APP=hello
-python3 -m flask run --port 5001 --host=0.0.0.0
+python3 -m flask run --port 5001 --host=127.0.0.1
+
+# test cross-origin
+export FLASK_APP=hello-cors
+python3 -m flask run --port 5001 --host=127.0.0.1
+
+# notes
+https://stackoverflow.com/questions/38742379/cors-why-my-browser-doesnt-send-options-preflight-request
--- hello-cors.py
+++ hello-cors.py
@@ -1,22 +1,24 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(app)
-#app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
 @app.route("/cors")
-#@cross_origin() # perchee errore se lo scommento???
+@cross_origin()
 def hello_world_cors():
     return "Hello, Cross Origin World!"
 
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-# https://stackoverflow.com/questions/63290047/flask-csp-content-security-policy-best-practice-against-attack-such-as-cross
 @app.after_request
 def add_security_headers(resp):
+    """
+    Allow use of developer console from "hello world" page to send requests
+    https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+    https://stackoverflow.com/questions/63290047/flask-csp-content-security-policy-best-practice-against-attack-such-as-cross
+    """
     resp.headers['Content-Security-Policy']='default-src \'self\' http://*; connect-src *;'
     return resp
